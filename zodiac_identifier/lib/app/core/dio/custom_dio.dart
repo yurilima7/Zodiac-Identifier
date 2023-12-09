aff1a94d515e95ca5f31b4ed467c77389f6f3ed5
Yuri Lima <61335084+yurilima7@users.noreply.github.com>
2023-12-09 16:53:05 -0300

    Teste de conexão

--- zodiac_identifier/lib/app/core/dio/custom_dio.dart
+++ zodiac_identifier/lib/app/core/dio/custom_dio.dart
@@ -5,7 +5,8 @@ class CustomDio extends DioForNative {
 
   CustomDio() : super (
     BaseOptions(
-      baseUrl: 'https://constellation-api.onrender.com',
+      // baseUrl: 'https://constellation-api.onrender.com',
+      baseUrl: 'https://constellation.yurilima1.repl.co',
       connectTimeout: const Duration(seconds: 5000),
       receiveTimeout: const Duration(seconds: 60000),
     ),
